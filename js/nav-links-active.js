9e6d8be4276ad45ec54a3ac86fe96f4f2d895652
eugeniucc <eugeniucoloteniucc@gmail.com>
2024-06-25 11:02:01 +0300

    save

--- js/nav-links-active.js
+++ js/nav-links-active.js
@@ -10,3 +10,16 @@ function linksActive() {
   });
 }
 document.addEventListener("DOMContentLoaded", linksActive);
+
+function linksActiveMobile() {
+  const currentUrl = window.location.href;
+  const links = document.querySelectorAll(".header_mobile-nav--links");
+  links.forEach((element) => {
+    if (element.href === currentUrl) {
+      element.classList.add("header_mobile-nav--links-active");
+    } else {
+      element.classList.remove("header_mobile-nav--links-active");
+    }
+  });
+}
+document.addEventListener("DOMContentLoaded", linksActiveMobile);
