1b4e8b4318eb6aa8765ec0541ee22d4cbb0193fb
eugeniucc <eugeniucoloteniucc@gmail.com>
2024-08-22 14:34:01 +0300

    save

--- js/burger-mobile.js
+++ js/burger-mobile.js
@@ -1,7 +1,11 @@
 const burger = document.querySelector(".header_mobile-burger");
 const navMenu = document.querySelector(".header_mobile-nav");
+const body = document.body;
+const navMenuOverlay = document.querySelector(".header__mobile-nav--overlay");
 
 burger.addEventListener("click", () => {
   burger.classList.toggle("active");
   navMenu.classList.toggle("nav-menu-show");
+  body.classList.toggle("freeze");
+  navMenuOverlay.classList.toggle("active");
 });
--- js/nike-rev.js
+++ js/nike-rev.js
@@ -1,14 +1,14 @@
-const sizesBtn = document.querySelectorAll(".nike__sizes-btn");
-const arrsizesBtn = [...sizesBtn];
-const colorsBtn = document.querySelectorAll(".nike__colors-btn");
-const arrcolorsBtn = [...sizesBtn];
+const [...sizesBtn] = document.querySelectorAll(".nike__sizes-btn");
+const [...colorsBtn] = document.querySelectorAll(".nike__colors-btn");
 
-arrsizesBtn.forEach((el) => {
+sizesBtn.forEach((el) => {
   el.addEventListener("click", () => {
     el.classList.toggle("active");
   });
 });
 
-// arrcolorsBtn.forEach((el) => {
-//   el.classList.toggle("active");
-// });
+colorsBtn.forEach((el) => {
+  el.addEventListener("click", () => {
+    el.classList.toggle("active");
+  });
+});
