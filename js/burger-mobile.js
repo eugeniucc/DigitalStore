9a57dec98922accc77ad337d22e1cd523d1f29ec
eugeniucc <eugeniucoloteniucc@gmail.com>
2024-08-23 21:44:24 +0300

    save

--- js/burger-mobile.js
+++ js/burger-mobile.js
@@ -15,6 +15,7 @@ navMenuOverlay.addEventListener("click", (e) => {
   navMenu.classList.remove("nav-menu-show");
   body.classList.remove("freeze");
   navMenuOverlay.classList.remove("active");
+  filterProducts.classList.remove("active");
 });
 
 document.addEventListener("keydown", (e) => {
@@ -23,5 +24,25 @@ document.addEventListener("keydown", (e) => {
     navMenu.classList.remove("nav-menu-show");
     body.classList.remove("freeze");
     navMenuOverlay.classList.remove("active");
+    filterProducts.classList.remove("active");
   }
+});
+
+// Products html
+
+const filterButton = document.querySelector(".dropdown__category");
+const filterProducts = document.querySelector(".sidebar__mobile");
+
+filterButton.addEventListener("click", () => {
+  filterProducts.classList.toggle("active");
+  navMenuOverlay.classList.toggle("active");
+  body.classList.toggle("freeze");
+});
+
+const sidebarMobileClose = document.querySelector(".sidebar_filter-close");
+
+sidebarMobileClose.addEventListener("click", () => {
+  filterProducts.classList.remove("active");
+  navMenuOverlay.classList.remove("active");
+  body.classList.remove("freeze");
 });
